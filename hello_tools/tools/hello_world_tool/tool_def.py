5b9a22df7bb2ecf5f483795ece9c7a7928f94d28
michaelkubi <michael.bauer@kubiya.ai>
2025-01-30 13:36:38 +0200

    added private registry

--- hello_tools/tools/hello_world_tool/tool_def.py
+++ hello_tools/tools/hello_world_tool/tool_def.py
@@ -1,6 +1,6 @@
 import inspect
 
-from kubiya_sdk.tools.models import Arg, Tool, FileSpec
+from kubiya_sdk.tools.models import Arg, Tool, FileSpec, ImageProvider, Auth
 from kubiya_sdk.tools.registry import tool_registry
 
 from . import main
@@ -11,6 +11,13 @@ hello_tool = Tool(
     image="python:3.12",
     description="Prints hello {name}!",
     args=[Arg(name="name", description="name to say hello to", required=True)],
+    image_provider=ImageProvider(
+        kind="dockerhub",
+        auth=[
+            Auth(name="username", value="dmichaelb"), 
+            Auth(name="password", value_from={"secret": "MICHAEL_DH_PASSWORD"})
+        ],
+    ),
     on_build="""
 curl -LsSf https://astral.sh/uv/0.4.27/install.sh | sh > /dev/null 2>&1
 . $HOME/.cargo/env
@@ -31,6 +38,7 @@ python /tmp/main.py $name
             content=inspect.getsource(main),
         ),
     ],
+    secrets=["MICHAEL_DH_PASSWORD"],
 )
 
 tool_registry.register("hello_world_tool", hello_tool)
